26656884d77c758837206edc85969cc16bf7a7c7
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-20 19:58:49 +0530

    add .env.production file

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 
+if (process.env.REACT_APP_SENTRY_DSN) {
 Sentry.init({
   dsn: "https://010025556381e1995d43dfcdedd1ebee@o4505862073024512.ingest.sentry.io/4505913195823104",
   integrations: [
@@ -20,6 +21,7 @@ Sentry.init({
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
